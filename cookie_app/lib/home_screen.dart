e56f982772b09cd76b359dab092b1463784874fb
Elizabeth-Mathew <62259180+Elizabeth-Mathew@users.noreply.github.com>
2020-09-08 22:47:54 +0530

    flutter commit #2

--- cookie_app/lib/home_screen.dart
+++ cookie_app/lib/home_screen.dart
@@ -8,33 +8,58 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(68.0), // here the desired height
-        child: AppBar(
-          backgroundColor: Colors.blue,
-          leading: Builder(
-            builder: (BuildContext context) {
-              return IconButton(
-                icon: const Icon(Icons.person),
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                },
-                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-              );
-            },
+    return SafeArea(
+      child: Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(68.0), // here the desired height
+          child: AppBar(
+            backgroundColor: Colors.blue,
+            leading: Builder(
+              builder: (BuildContext context) {
+                return IconButton(
+                  icon: const Icon(Icons.person),
+                  onPressed: () {
+                    Scaffold.of(context).openDrawer();
+                  },
+                  tooltip:
+                      MaterialLocalizations.of(context).openAppDrawerTooltip,
+                );
+              },
+            ),
+            actions: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(left: 10, bottom: 10),
+                child: IconButton(
+                  icon: Icon(Icons.menu),
+                  tooltip: 'Show Photo',
+                  onPressed: () {},
+                ),
+              ),
+            ],
+            title: Text('Your space'),
           ),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.menu),
-              tooltip: 'Show Photo',
-              onPressed: () {},
+        ),
+        body: Stack(
+          children: <Widget>[
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: FloatingActionButton(
+                onPressed: () {},
+                child: Icon(Icons.chat_bubble),
+                backgroundColor: Colors.blue,
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: FloatingActionButton(
+                onPressed: () {},
+                child: Icon(Icons.chat_bubble),
+                backgroundColor: Colors.blue,
+              ),
             ),
           ],
-          title: Text('Your space'),
         ),
       ),
-      body: Stack(),
     );
   }
 }
